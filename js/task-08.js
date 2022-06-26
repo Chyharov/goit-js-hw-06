9506835b9c2dffcc488eb471540ba8ea9f1e0d69
Oleksii Chyharov <chyharov@gmail.com>
2022-06-27 01:41:35 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const {email, password} = event.target;
+
+    if (email.value === "" || password.value === "") {
+        return alert("Все поля должны быть заполнены.");
+    }
+
+    const userForm = {
+        email: email.value,
+        password: password.value,
+    };
+
+    console.log(userForm)
+    event.target.reset();
+})
